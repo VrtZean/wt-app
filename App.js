6b15c1681bcef523806bd6ded7ae360b419c1a6a
Varintorn Khomongkonudom <43430799+VrtZean@users.noreply.github.com>
2018-10-04 10:37:53 +0700

    finish

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+<<<<<<< HEAD
+import WeatherScreen from './components/WeatherScreen';
+import ZipCodeScreen from './components/ZipCodeScreen';
+=======
 import Weather from './components/Weather'
 import WeatherScreen from'./components/WeatherScreen'
 import ZipCodeScreen from'./components/ZipCodeScreen'
+>>>>>>> 82c63c62fbede188765518f31a4f40db7ab58ec5
 import { createStackNavigator } from 'react-navigation';
 
 const RootStack = createStackNavigator({
  Weather: WeatherScreen,
  ZipCode: ZipCodeScreen
 },{
- initialRouteName: 'ZipCode',
-})
+    initialRouteName: 'Weather',
+    initialRouteParams: {zipCode: '90110'}
+   })
+   
 export default class App extends React.Component {
  render() {
  return (
